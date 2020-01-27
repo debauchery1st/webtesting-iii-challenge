32f826781a3b0d32530d24c6efbdf27c8cfcff9c
debauchery1st <trevor.j.martin@gmail.com>
2020-01-27 15:02:03 -0500

    basic component tests

--- src/components/controls/Controls.test.js
+++ src/components/controls/Controls.test.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Controls from "./Controls";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<Controls />);
+  const linkElement = getByText(/Close Gate/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- src/components/dashboard/Dashboard.test.js
+++ src/components/dashboard/Dashboard.test.js
@@ -1,1 +1,10 @@
-// Test away
+// Test away// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<Dashboard />);
+  const linkElement = getByText(/Unlocked/i);
+  expect(linkElement).toBeInTheDocument();
+});
